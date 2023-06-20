ad867139b35432d4e2e0172c99073495738871b5
PeterJackNaylor <peter.jack.naylor@gmail.com>
2023-06-20 19:00:14 +0200

    opt wires

--- inr_src/hyperparameter_tuning.py
+++ inr_src/hyperparameter_tuning.py
@@ -6,7 +6,7 @@ def add_config_optuna_to_opt(opt, trial):
     model_hp = AttrDict()
     model_hp.fourier = opt.fourier
     model_hp.siren = opt.siren
-    model_hp.wires = opt.siren
+    model_hp.wires = opt.wires
     model_hp.verbose = opt.p.verbose
     model_hp.epochs = opt.p.epochs
 
