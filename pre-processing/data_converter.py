b146c8bc1e5e2adf14dd65a9ad3a7049e8dd0984
PeterJackNaylor <peter.jack.naylor@gmail.com>
2023-11-16 13:36:05 +0100

    updated

---
+++ pre-processing/data_converter.py
@@ -0,0 +1,91 @@
+import argparse
+from os.path import join
+import xarray
+import numpy as np
+import pandas as pd
+from glob import glob
+from tqdm import tqdm
+from datetime import datetime
+
+
+def parser_f():
+    parser = argparse.ArgumentParser(
+        description="data_converter",
+    )
+    parser.add_argument(
+        "--data_path",
+        type=str,
+    )
+    parser.add_argument(
+        "--data_setup",
+        type=str,
+        help="Defines which pre-defined setup for the data to use",
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+def set_time(time_array):
+    date_2000 = pd.Timestamp(datetime(2000, 1, 1))
+    start_time = pd.Timestamp(datetime(2010, 7, 1))
+    time_array = (time_array + date_2000 - start_time) / np.timedelta64(1, "D")
+    return time_array
+
+
+def get_dataset_from_xarray(path, swath_id=0):
+    xa = xarray.open_dataset(path)
+    df = xa.to_dataframe()
+    df["Swath"] = swath_id
+    df = df[["Lon", "Lat", "Height", "Time", "Swath", "Coherence"]]
+    df = df[df.index.get_level_values("d2") == 0]
+    df = df.reset_index(drop=True)
+    df.Time = set_time(df.Time)
+    return df.to_numpy()
+
+
+def PetermannGlacier(path, year_start=2010, year_end=2022, month_start=7, month_end=12):
+    files = []
+    last_month = 12
+    for year in range(year_start, year_end + 1):
+        if year != year_start:
+            month_start = 1
+        if year == year_end:
+            last_month = month_end
+        for month in range(month_start, last_month):
+            files += glob(join(path, f"{year}/{month:02d}/*.nc"))
+    id_ = 0
+    list_arrays = []
+    for f in tqdm(files):
+        array = get_dataset_from_xarray(f, swath_id=id_)
+        list_arrays.append(array)
+        id_ += 1
+    full_data = np.concatenate(list_arrays, axis=0)
+    np.save("data.npy", full_data)
+
+
+def main():
+    options = parser_f()
+
+    data_path = options.data_path
+    data_setup = options.data_setup
+
+    if data_setup == "mini":
+        year_start, year_end = 2016, 2016
+        month_start, month_end = 1, 3
+    elif data_setup == "small":
+        year_start, year_end = 2011, 2011
+        month_start, month_end = 1, 12
+    elif data_setup == "medium":
+        year_start, year_end = 2011, 2012
+        month_start, month_end = 1, 12
+    elif data_setup == "all":
+        year_start, year_end = 2010, 2022
+        month_start, month_end = 7, 12
+
+    PetermannGlacier(data_path, year_start, year_end, month_start, month_end)
+
+
+if __name__ == "__main__":
+    main()
---
+++ pre-processing/preprocess.py
@@ -0,0 +1,155 @@
+import argparse
+from pinns import read_yaml
+import numpy as np
+import pyproj
+import scipy
+from tqdm import tqdm
+
+
+def parser_f():
+    parser = argparse.ArgumentParser(
+        description="Preprocess",
+    )
+    parser.add_argument(
+        "--data",
+        type=str,
+    )
+    parser.add_argument("--params", type=str, help="Yaml file path")
+
+    args = parser.parse_args()
+    args.p = read_yaml(args.params)
+
+    return args
+
+
+def load():
+    pass
+
+
+def save():
+    pass
+
+
+def exclude_outliers(
+    data: np.array,
+    std_multiple: float,
+    time_step: int,
+    grid_size_m: int,
+    grid_overlap_m: int,
+):
+    pass
+    # data format: [lat, lon, z, t, coherence, swath_id]
+
+    # Define the projection (Stereographic projection centered on the North Pole)
+    proj = pyproj.Proj(proj="stere", lat_0=90, lon_0=0)
+
+    points = np.copy(data)
+    points[:, 0:2] = proj(data[:, 1], data[:, 0])
+
+    # Define the grid
+    x_min, y_min = points[:, 0].min(), points[:, 1].min()
+    x_max, y_max = points[:, 0].max(), points[:, 1].max()
+
+    # Create the grid
+    x_coords = np.arange(x_min, x_max, grid_size_m[0] - grid_overlap_m[0])
+    y_coords = np.arange(y_min, y_max, grid_size_m[1] - grid_overlap_m[1])
+    grid_points = np.array([(x, y) for y in y_coords for x in x_coords])
+
+    # Create a cKDTree object
+    tree = scipy.spatial.cKDTree(grid_points)
+
+    outliers = np.zeros_like(points.shape[0])
+
+    cells_with_0 = 0
+    cells_with_1 = 0
+
+    for t in tqdm(range(data[:, 3].max(), time_step)):
+        # Find the nearest grid point for each point within t - time_step
+        within_t = np.logical_and(points[:, 3] > t, points[:, 3] <= t - time_step)
+        points_within_t = points[within_t]
+        _, indices = tree.query(points_within_t[:, :2])
+
+        # Initialize arrays for the mean and std of each cell
+        mean_values = np.zeros(len(grid_points))
+        std_values = np.zeros(len(grid_points))
+
+        # For each grid point (cell)
+        for i, grid_point in enumerate(tqdm(grid_points)):
+            # Determine the boundaries of the current cell
+            cell_x_min, cell_y_min = grid_point - grid_overlap_m
+            cell_x_max, cell_y_max = grid_point + grid_overlap_m
+
+            # Get the points within the current cell
+            cell_points = points_within_t[
+                (points_within_t[:, 0] >= cell_x_min)
+                & (points_within_t[:, 0] < cell_x_max)
+                & (points_within_t[:, 1] >= cell_y_min)
+                & (points_within_t[:, 1] < cell_y_max)
+            ]
+
+            # Calculate the mean and std of the points within the current cell
+            if len(cell_points) == 0:
+                cells_with_0 += 1
+                mean_values[i] = np.nan
+                std_values[i] = np.nan
+            elif len(cell_points) == 1:
+                cells_with_1 += 1
+                mean_values[i] = cell_points[:, 2]
+                std_values[i] = cell_points[:, 2]
+            else:
+                mean_values[i] = np.mean(cell_points[:, 2])
+                std_values[i] = np.std(cell_points[:, 2])
+
+        # Now you can create a boolean array that indicates whether each point is within three standard deviations of the mean of its cell
+        within_threshold = np.zeros(points_within_t.shape[0], dtype=bool)
+
+        # Check if the point is within three standard deviations of the mean of its cell
+        within_threshold = np.all(
+            (
+                points_within_t[:, 2]
+                >= mean_values[indices] - std_multiple * std_values[indices]
+            )
+            & (
+                points_within_t[:, 2]
+                <= mean_values[indices] + std_multiple * std_values[indices]
+            )
+        )
+
+        outliers[points_within_t][~within_threshold]
+
+    data = data[~outliers]
+    print(
+        f"number of points left: {np.sum(outliers)} out of {points.shape[0]}, a reduction of {points.shape[0] - np.sum(outliers)} points"
+    )
+
+    return data, outliers
+
+
+def exclude_coherence(data: np.array, coherence_threshold: float = 0.6):
+    # data format: [lat, lon, z, t, coherence, swath_id]
+    below_threshold = data[:, 4] < coherence_threshold
+    data = data[below_threshold]
+
+    return data, below_threshold
+
+
+def main():
+    options = parser_f()
+    data = np.load(options.data)
+
+    if options.exclude_coherence:
+        options.exclude_grid_size_m = np.array((10, 10)) * 1000
+        options.exclude_grid_overlap_m = options.exclude_grid_size_m // 2
+        data, _ = exclude_coherence(
+            data=data,
+            coherence_threshold=options.coherence_threshold,
+            grid_size_m=options.exclude_grid_size_m,
+            grid_overlap_m=options.exclude_grid_overlap_m,
+        )
+
+    if options.exclude_outliers:
+        data, _ = exclude_outliers(data=data, std_multiple=options.exclude_std_multiple)
+
+
+if __name__ == "__main__":
+    main()
